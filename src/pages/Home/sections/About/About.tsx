8c313477be616ea03e65250ab3e63b8fa2afe253
Amanda Silva <amandadasilvapereira162004@gmail.com>
2026-03-26 01:51:56 -0300

    About started

---
+++ src/pages/Home/sections/About/About.tsx
@@ -0,0 +1,28 @@
+import { Container, Grid, styled, Typography } from "@mui/material"
+
+
+const NavBar = () => {
+
+    const StyledAbout = styled("div")(({theme})=> ({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        
+    }))
+
+    return (
+        <>
+            <Container maxWidth="lg">
+                <StyledAbout>
+                    <Grid size={{ xs: 12, md: 4 }} >
+                            <Typography color="#3a1636" variant="h6" textAlign="center" pt={1} >"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</Typography>
+                    </Grid>
+                </StyledAbout>
+            </Container>
+            
+        </>
+    )
+}
+
+export default NavBar
